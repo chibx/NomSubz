a1e23db96e6c0e8795579ef5adf33b33c4656fb8
chibx <111882496+chibx@users.noreply.github.com>
2026-07-02 11:59:54 +0100

    Add response types

--- app/api/lib/types/response.ts
+++ app/api/lib/types/response.ts
@@ -1,9 +1,52 @@
+import { InferEnum } from "drizzle-orm";
 import { ValidationError } from "../utils/utils";
-import { NullT } from "./types";
+import { NullT, PlanType } from "./types";
+import { subscriptionsStatusEnum } from "../db/schema";
 
 export type StructuredResponse<T = unknown> = {
     status: number;
     message: string;
     data: NullT<T>;
     errors?: ValidationError[];
+};
+
+/** POST /api/customers */
+export type CreateSubscriberResponse = {
+    /** Subscriber is a bigint (int64) string */
+    subscriberId: string;
+};
+
+/** GET /api/customers/[id] */
+export type GetSubscriberResponse = {
+    /** Subscriber is a bigint (int64) string */
+    subscriberId: string;
+    appId: string;
+    userId: string;
+    createdAt: string;
+    deletedAt: string | null;
+};
+
+export type SubscriberCard = {
+    id: string;
+    subscriberId: bigint;
+    brand: string;
+    last4: string;
+    expiryMonth: number;
+    expiryYear: number;
+    createdAt: string;
+};
+
+export type GetSubscriberCardsResponse = {
+    cards: SubscriberCard[];
+};
+
+export type GetSubscriptionResponse = {
+    id: string;
+    amount: string;
+    createdAt: Date;
+    startTime: Date;
+    endTime: Date;
+    status: InferEnum<typeof subscriptionsStatusEnum>;
+    planName: string;
+    planType: PlanType;
 };

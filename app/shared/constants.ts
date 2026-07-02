4df54077576b29fa799219ea99fa1b503022fa16
chibx <111882496+chibx@users.noreply.github.com>
2026-07-02 12:26:39 +0100

    Minor import modification

--- app/shared/constants.ts
+++ app/shared/constants.ts
@@ -1,5 +1,5 @@
 import { FormDataInfo } from "decode-formdata";
-import { AnalyticsPeriodType } from "../api/lib/types/types";
+import { AnalyticsPeriodType } from "@/app/api/lib/types/types";
 
 export const APP_NAME = "NomSubz";
 export const CALLBACK_URL = "https://numsubz.vercel.app/";

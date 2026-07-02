348375ddf1f150a1620335c0bb663af748558881
chibx <111882496+chibx@users.noreply.github.com>
2026-07-02 09:48:58 +0100

    Add shared constants between the frontend and backend

---
+++ app/shared/constants.ts
@@ -0,0 +1,20 @@
+import { FormDataInfo } from "decode-formdata";
+import { AnalyticsPeriodType } from "../api/lib/types/types";
+
+export const APP_NAME = "NomSubz";
+export const CALLBACK_URL = "https://numsubz.vercel.app/";
+export const ERR_EXPIRED_SESSION = "Expired session data";
+export const ERR_NO_ACCESSTOKEN = "No access token";
+export const ERR_INVALID_ACCESSTOKEN = "Invalid token";
+export const ERR_FIRST_UNBIND_CARD =
+    "This card is currently linked to a subscription, bind those subscriptions to another card or cancel subscription";
+
+const DAILY: AnalyticsPeriodType = 0;
+const WEEKLY: AnalyticsPeriodType = 1;
+const MONTHLY: AnalyticsPeriodType = 2;
+
+export { DAILY, WEEKLY, MONTHLY };
+
+export const REGISTER_FORM_INFO: FormDataInfo = {
+    booleans: ["receive_newsletter"],
+};

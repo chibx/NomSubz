25e869023d817eb21c6dae9c3d6d7fa41b0c523f
chibx <111882496+chibx@users.noreply.github.com>
2026-07-03 02:14:12 +0100

    New Commit

--- drizzle-prod.config.ts
+++ drizzle-prod.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
   schema: './app/api/lib/db/schema.ts',
-  out: './app/api/lib/db/migrations',
+  out: './app/api/lib/db/prod/migrations',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.PROD_DATABASE_URL!,
-    ssl: "prefer"
+    ssl: true
   },
 })

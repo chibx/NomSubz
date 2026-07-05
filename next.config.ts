bde611ce1bebeceec6bb5692630c722c8cf5881e
Chiboy <chiboyekene1@gmail.com>
2026-07-05 04:50:51 +0100

    Discard changes to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://nomsubz.vercel.app/api/:path*",
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
